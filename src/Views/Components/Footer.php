3625892ecec8611ae59e2648df35d8f039a3fc13
Sara <sarap120298@gmail.com>
2022-07-27 11:45:26 +0200

    changing footer

--- src/Views/Components/Footer.php
+++ src/Views/Components/Footer.php
@@ -5,7 +5,7 @@
 <footer class="sticky-footer text-white">
     <div class="container">
         <div class="copyright text-center my-auto">
-            <h4>Copyright &copy; Divers@s 2021</h4>
+            <h4>Copyright &copy; Fem-Desk 2022</h4>
         </div>
     </div>
 </footer>
