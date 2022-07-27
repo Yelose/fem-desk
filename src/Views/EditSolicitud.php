b1d681a7fac6071dc910bd8a8b4860f9eaa00c20
Marina González Suárez <yelose85@gmail.com>
2022-07-27 14:34:05 +0200

    installed sass, changed some classes

--- src/Views/EditSolicitud.php
+++ src/Views/EditSolicitud.php
@@ -5,30 +5,30 @@ require_once("Components/Header.php");
 
 <body>
     <div class="edit row justify-content-center">
-        <main class="card" style="width: 80%; height:27rem; display:flex; justify-content:center; ">
+        <main class="card">
 
-            <h2 class="text-center" style="margin-bottom: 3rem; margin-top: 1rem;">Edit appointment</h2>
+            <h2 class="text-center">Editar Solicitud</h2>
             <a href="./index.php"><button type="button" id="button-cancel" class="btn btn-outline-danger button-cancel">Cancel</button></a>
 
 
             <form class="justify-content-center " action='?action=update&id=<?php echo $data["solicitud"]->getId() ?>' method="post">
-                <div class="input-group" style=" margin-bottom: 2rem; width: 90%; margin-left: 5%;">
+                <div class="input-group">
                     <span class="input-group-text">Departamento:</span>
                     <input class="form-control" type="text" name="departamento" value='<?php echo $data["solicitud"]->getDepartamento() ?>'>
                 </div>
-                <div class="input-group" style=" margin-bottom: 2rem; width: 90%; margin-left: 5%;">
+                <div class="input-group">
                     <span class="input-group-text">Nombre:</span>
                     <input class="form-control" type="text" name="nombre" required value='<?php echo $data["solicitud"]->getNombre() ?>'>
                 </div>
-                <div class="input-group" style=" margin-bottom: 2rem; width: 90%; margin-left: 5%;">
+                <div class="input-group">
                     <span class="input-group-text">Consulta:</span>
                     <input class="form-control" type="text" name="consulta" required value='<?php echo $data["solicitud"]->getConsulta() ?>'>
                 </div>
-                <div class="input-group" style="width: 90%; margin-left: 5%;">
+                <div class="input-group">
                     <span class="input-group-text">Descripción:</span>
                     <input class="form-control" area-label="With textarea" type="textarea" name="descripcion" required value='<?php echo $data["solicitud"]->getDescripcion() ?>'>
                 </div>
-                <div class="botones" style="margin-bottom: 2rem;">
+                <div class="botones">
                     <input type="submit" value="Guardar">
                     <input type="reset" value="Reiniciar">
                 </div>
