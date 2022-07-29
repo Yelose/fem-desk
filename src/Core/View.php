9067778d9402bbb65f4282ea56b6c7754e7f9558
Estherherrera <graxyherrera@gmail.com>
2022-07-29 10:42:43 +0200

    test

--- src/Core/View.php
+++ src/Core/View.php
@@ -4,8 +4,15 @@ namespace App\Core;
 
 class View
 {
+    private $view;
     public function __construct(string $view, array $data = null)
     {
+        $this->view = $view;
        require_once("src/Views/$view.php");
+    }
+
+    public function getView()
+    {
+        return $this->view;
     }
 }
